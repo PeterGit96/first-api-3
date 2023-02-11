fe35ec69fc80a7da562252bfc356b9cedfcf2f18
Pietro Gallina <pietrogallina96@gmail.com>
2023-02-11 19:22:27 +0100

    Initial commit

---
+++ src/main/java/co/develhope/firstapi3/controllers/StringController.java
@@ -0,0 +1,20 @@
+package co.develhope.firstapi3.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/strings")
+public class StringController {
+
+    @GetMapping
+    public String concatenateStrings(@RequestParam String s1, @RequestParam(required = false) String s2) {
+        if(s2 == null) {
+            return s1;
+        }
+        return s1 + s2;
+    }
+
+}
